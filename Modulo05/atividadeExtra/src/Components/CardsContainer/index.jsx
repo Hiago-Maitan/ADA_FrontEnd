9c885499f44af205b67e0500d0de6f09cc697b38
Maitann <hiagomaitan@gmail.com>
2024-02-01 16:51:09 -0300

    FEAT: dinamic cards

--- Modulo05/atividadeExtra/src/Components/CardsContainer/index.jsx
+++ Modulo05/atividadeExtra/src/Components/CardsContainer/index.jsx
@@ -1,74 +1,26 @@
+import { gamesArray } from "../../GamesArray";
+
 function CardsContainer() {
   return ( 
-    <div className="articleContainer" >
+  <div className="articleContainer" >
     <h1>Ofertas</h1>
   {/* Galeria dos jogos */}
+ { gamesArray.map( data => (
+  <>
     <article>
       <figure>
-        <img src="starWarsJedi.webp" alt="Imagem do Jogo 1"/>
-        <figcaption>STAR WARS Jedi: Survivor</figcaption>
+        <img src={data.src} alt={"Imagem do jogo " + data.name} />
+        <figcaption>{data.name}</figcaption>
       </figure>
-      <div className="tag">-35%</div>
+      <div className="tag">{data.discount}</div>
       <div className="price">
-        <span>R$ 200,28</span>
-        <span className="oldPrice">R$ 338,90</span>
+        <span>{data.price}</span>
+        <span className="oldPrice">{data.oldPrice}</span>
       </div>
     </article>
-    <article>
-      <figure>
-        <img src="gta5.webp" alt="Imagem do Jogo 1"/>
-        <figcaption>Grand Theft Auto IV</figcaption>
-      </figure>
-      <div className="tag">-33%</div>
-      <div className="price">
-        <span>R$ 107,13</span>
-        <span className="oldPrice">R$ 159,00</span>
-      </div>
-    </article>
-    <article>
-      <figure>
-        <img src="legoStarWars.webp" alt="Lego Star Wars: A saga skywalker"/>
-        <figcaption>Lego Star Wars</figcaption>
-      </figure>
-      <div className="tag">-67%</div>
-      <div className="price">
-        <span>R$ 75,89</span>
-        <span className="oldPrice">R$ 229,99</span>
-      </div>
-    </article>
-    <article>
-      <figure>
-        <img src="dragonBall.webp" alt="Dragon Ball Z: Kakarot"/>
-        <figcaption>Dragon Ball Z: Kakarot</figcaption>
-      </figure>
-      <div className="tag">-75%</div>
-      <div className="price">
-        <span>R$ 74,97</span>
-        <span className="oldPrice">R$ 299,90</span>
-      </div>
-    </article>
-    <article>
-      <figure>
-        <img src="civilization.jpeg" alt="Sid Meier`s Civilization VI"/>
-        <figcaption>Descrição do Jogo 1</figcaption>
-      </figure>
-      <div className="tag">-80%</div>
-      <div className="price">
-        <span>R$ 29,90</span>
-        <span className="oldPrice">R$ 149,50</span>
-      </div>
-    </article>
-    <article>
-      <figure>
-        <img src="turtle.webp" alt="Teenage Mutant Ninja Turtles"/>
-        <figcaption>Teenage Mutant Ninja Turtles</figcaption>
-      </figure>
-      <div className="tag">-30%</div>
-      <div className="price">
-        <span>R$ 87,15</span>
-        <span className="oldPrice">R$ 124,50</span>
-      </div>
-    </article>
+  </>
+  ))
+ }
 {/* FIM DA GALERIA */}
   </div>
    );
---
+++ Modulo05/atividadeExtra/src/GamesArray.jsx
@@ -0,0 +1,43 @@
+export const gamesArray = [
+  {
+    name: "Star Wars - Jedi: Survivor",
+    src: "starWarsJedi.webp",
+    discount: "-35%",
+    price: 200.28,
+    oldPrice: 338.90
+  },
+  {
+    name: "Grand Theft Auto V",
+    src: "gta5.webp",
+    discount: "-45%",
+    price: 249.90,
+    oldPrice: 137.445
+  },{
+    name: "Lego Star Wars",
+    src: "legoStarWars.webp",
+    discount: "-75%",
+    price: 399.90,
+    oldPrice: 499.90
+  },
+  {
+    name: "Civilization VI",
+    src: "civilization.jpeg",
+    discount: "-10%",
+    price: 119.99,
+    oldPrice: 139.99
+  },
+  {
+    name: "Teenage Turtles",
+    src: "turtle.webp",
+    discount: "-10%",
+    price: 119.99,
+    oldPrice: 139.99
+  },
+  {
+    name: "Dragon Ball Z",
+    src: "dragonBall.webp",
+    discount: "-25%",
+    price: 149.99,
+    oldPrice: 199.99
+  }  
+]
