e8c8d7c6a0e45e0100735deccaa75f7c29b50c35
Maitann <hiagomaitan@gmail.com>
2024-02-01 00:49:58 -0300

    add exercicio 02

---
+++ Modulo05/ex02/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './Components/Header'
+
+function App() {
+  return (
+  <>
+  <Header/>
+   <section id="news">
+    <img src="games.jpg" alt="Capa de Novidades"/>
+   </section>
+  </>
+  )
+}
+
+export default App;
